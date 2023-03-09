97858a8652c37456b8c5daa6250ceb97905c0447
Félix Martínez <123627867+felixmaral@users.noreply.github.com>
2023-03-09 18:25:57 +0100

    calc

---
+++ P2/S4/calc0-02.js
@@ -0,0 +1,11 @@
+console.log("Ejecutando JS...");
+
+//-- Elementos de la interfaz de la calculadora
+display = document.getElementById("display")
+boton1 = document.getElementById("boton1")
+boton2 = document.getElementById("boton2")
+suma = document.getElementById("suma")
+igual = document.getElementById("igual")
+clear = document.getElementById("clear")
+
+console.log(display)
