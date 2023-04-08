dca660ff575bb33b1bbc883ef45133d1b13ece40
Félix Martínez Alonso <f.martineza.2019@alumnos.urjc.es>
2023-04-09 01:17:24 +0200

    avances

--- P3/Animaciones/vel.js
+++ P3/Animaciones/vel.js
@@ -86,10 +86,26 @@ var start = document.getElementById("start"); // Button Start New Game
 
 // Funciones de juego
 
+function calcularVectores(a, v) {
+    radians = ((a*Math.PI)/180);
+    vx = v * Math.cos(radians);
+    vy = v * Math.sin(radians);
+
+    return {vx, vy};
+}
+
 function dibujarProyectil() {
     const ctx = canvas.getContext('2d');
     ctx.fillStyle = 'green';
-    ctx.fillRect(10, canvas.height - 20, 18, 18);
+    ctx.fillRect(5, canvas.height - 20, 15, 15);
+}
+
+function posicionDisparo(vx, vy, t) {
+    g = -9,8;
+    px = 5 + vx*t 
+    py = ((canvas.height - 20) - vy*t - (1/2)*g*(t**2))
+
+    return px, py;
 }
 
 function dibujarCirculoAleatorio(x, y) {
@@ -122,23 +138,56 @@ function reset() {
     }   
 }
 
-function shootF(x, y) {
-  
-    x = x + velp;
+function shootF() {
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    dibujarCirculoAleatorio(x, y);
-    dibujarProyectil(x, y, 10, 10, "blue"); // Pintar el proyectil
+    
+    t = t + 0.1;
+
+    // Calcular nuevas posiciones
+    calcularVectores(angulo.value, vel.value);
+    posicionDisparo(vx, vy, t);
+
+    // Detectar colisiones con las paredes laterales
+    if (px >= canvas.width - 15) {
+        px = canvas.width - 15;
+        vx = -vx * 0.8;
+    } else if (px <= 0) {
+        px = 0;
+        vx = -vx * 0.8;
+    }
+
+    // Detectar colisiones con el techo y el suelo
+    if (py >= canvas.height - 15) {
+        py = canvas.height - 15;
+        vy = 0; // Invertir la velocidad en el eje y y reducirla por un factor de 0.8 (coeficiente de restitución)
+        vx = 0;
+    } else if (py <= 0) {
+        py = 0;
+        vy = -vy * 0.8; // Invertir la velocidad en el eje y y reducirla por un factor de 0.8 (coeficiente de restitución)
+        vx = vx * 0.8;
+    }   
+
+    // Dibujar el círculo rojo
+    dibujarCirculoAleatorio(x, y); 
+
+    // Dibujar el proyectil verde
+    ctx.fillStyle = 'green';
+    ctx.fillRect(px, py, 15, 15);
+
     requestAnimationFrame(shootF);
 
 }
+
 
 // Main
 
 function main() {
 
     timer = new Crono(cronoElements.display);
-
-    velp = vel.value;
+    
+    calcularVectores(angulo.value, vel.value);
+    t = 0;
     x = Math.floor((Math.random() * 0.73 + 0.27) * (canvas.width - 25)) + 10; // Ajuste para evitar que el círculo se corte en los bordes
     y = Math.floor(Math.random() * (canvas.height - 25)) + 10; // Ajuste para evitar que el círculo se corte en los bordes
 
@@ -150,8 +199,9 @@ function main() {
 
     reset(); // Activa la funcionalidad del boton Start New Game
     shoot.onclick = () => {
-        shootF(ang.value,vel.value)
+        shootF();
     }
+
 }
 
 // Esperar a que cargue el documento para iniciar
